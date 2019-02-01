34ebacceae98e1cee2cb2614c1425b70630cc23a
Azned <aznedazned@gmail.com>
2019-02-01 16:30:48 +0400

    big upd

--- main.cpp
+++ main.cpp
@@ -36,20 +36,50 @@ const short hmap=25,wmap=40; //size map
 
 class Game{
     public:
-    Game(){
 
-        window =new sf::RenderWindow(sf::VideoMode(1000, 700), "shake");
+    Game(int td=0){
+        timedelay=td;
+    }
 
+    void setup(){
+        int log=0;
+        std::cout<<"Log in console? ";
+        std::cin>>log;
+
+        if (log==0){
+            std::clog.setstate(std::ios_base::failbit);
+        }
+
+        int timedelay;
+        std::cout<<"Enter time delay. (during program work use left mouse button to increase time delay, right to decrease)  ";
+        std::cin>>timedelay;
+        std::cout<<std::endl;
+        settimedelay(timedelay);
+    }
+
+    void setupwindow(int weight=1000,int height=700){
+        window = new sf::RenderWindow(sf::VideoMode(weight, height), "shake");
     }
 
     sf::RenderWindow* getwindow(){return window;}
+    int gettimedelay(){return timedelay;}
+    void settimedelay(int td){if(td>=0){timedelay=td;}else{timedelay=0;}}
+
+    ~Game(){
+        delete window;
+    }
+
+    private:
+    int timedelay=0;
     sf::RenderWindow* window;
 };
 class World {
     public:
     int update(std::string *Map){
-        Map[1 + rand() % (hmap-2)][1 + rand() % (wmap-2)]='f';
-        return 0;
+        int y =1 + rand() % (hmap-2);
+        int x =1 + rand() % (wmap-2);
+        if (Map[y][x]==' ')
+            Map[y][x]='f';
     }
 };
 
@@ -72,6 +102,7 @@ public:
     bool live=1, division=0;
     short static shakecounter;
     short x=0,y=0,up=0,down=0,left=0,right=0,lastx=0,lasty=0,shakeid,shakesize=5,saturation=0;
+    short hungryi=0;
 
     struct c {
     int x,y;
@@ -91,7 +122,7 @@ public:
     int getshaketaily(int i){return shaketail[i].y;}
 
     void divisionsuccess(std::string *Map,Shake &shake){
-        int j=3;
+        //int j=3;
         for (int i=shakesize-7;i<shakesize-2;i++){
             std::clog<<"y= "<<shaketail[i].y<<" x= "<<shaketail[i].x<<std::endl;
             Map[shaketail[i].y][shaketail[i].x]=' ';
@@ -99,7 +130,7 @@ public:
                 shake.shaketail[j].x=shaketail[i].x;
                 shake.shaketail[j].y=shaketail[i].y;
             }
-            j--;*///work in progress
+            j--;*/    //work in progress
         }
         shakesize-=5;
         division=0;
@@ -112,11 +143,18 @@ public:
         shakesize++;
     }
 
+    void delltail(std::string *Map){
+        if (Map[shaketail[shaketail.size()-2].y][shaketail[shaketail.size()-2].x]!='0')
+            Map[shaketail[shaketail.size()-2].y][shaketail[shaketail.size()-2].x]=' ';
+        shaketail.resize(shaketail.size()-1);
+        shakesize--;
+    }
+
     void update(std::string *Map){ //x- left x+ right y- up y+ down
         for (int i=0;i<7;i++){
             for (int j=0;j<7;j++){
                 if ((y-3+i)>=0&&(x-3+j)>=0){
-                    if ((y-3+i)<hmap&&(x-3+j)<wmap){//need test
+                    if ((y-3+i)<hmap&&(x-3+j)<wmap){
                         switch (Map[y-3+i][x-3+j]){
                             case 'f':
                             case 'd':
@@ -182,11 +220,21 @@ public:
 
         //Map[lasty][lastx]=' '; // temporarily
 
-        if (saturation>9){
+        if (saturation>12){
             saturation-=10;
             addtail();
         }
 
+        if (saturation==0){
+            saturation+=10;
+            delltail(Map);
+        }
+
+        if (shakesize<4){
+            live=0;
+        }
+
+        if (hungryi>5){hungryi=0;saturation--;}else{hungryi++;}
 
         if (shakesize>9){
             division=1;
@@ -198,7 +246,8 @@ public:
     }
 
     void fillshake(std::string *Map,char smbl){
-        //Map[y][x]=smbl;
+        if (Map[y][x]!='0')
+            Map[y][x]=smbl;
         for (auto i=0;i<shakesize-2;i++){
             Map[shaketail[i].y][shaketail[i].x]=smbl;
         }
@@ -214,66 +263,55 @@ class Shakescntrl{
     public:
     Shakescntrl(){
     shakes.resize(1);
-    /*shakes.resize(2);
-    shakes[1].x=10;
-    shakes[1].y=10;*/
     }
+
     void update(std::string *Map){
         std::clog<<"shakes size= "<<shakes.size();
         for (int i=0;i<shakes[0].getshakecounter();i++){
             if (shakes[i].getlive()){
                 shakes[i].update(Map);
 
-                if (shakes[i].getdivision()){//division
+                if (shakes[i].getdivision()){
                     shakes.resize(shakes[0].getshakecounter()+1);
-
                     shakes[shakes.size()-1].x=shakes[i].shaketail[shakes[i].getshakesize()-3].x;
                     shakes[shakes.size()-1].y=shakes[i].shaketail[shakes[i].getshakesize()-3].y;
-
-
-                    /*for (int i=(shakes[i].getshakesize()-2);i>5;i--){
-                        shakes[i+1].shaketail[i].y=shakes[i].getshaketaily(i);
-                        shakes[i+1].shaketail[i].x=shakes[i].getshaketailx(i);
-                        //std::clog<<std::setw(7)<<i<<std::setw(4)<<shaketail[i].y<<' '<<shaketail[i].x<<std::endl;
-                        Map[shakes[i+1].shaketail[i].y][shakes[i+1].shaketail[i].x]='s'; //error sigsegv
-                    }*/
                     shakes[i].divisionsuccess(Map,shakes[i+1]);
+
                 }
             }
             else{
                     shakes[i].fillshake(Map,'d');
                     //shakes[i].~Shake();
-                    shakes.erase_element(i);
-                    shakes[0].shakedie();// уменьшеает shakecounter
+                    shakes.erase(shakes.begin()+i);
+                    shakes[0].shakedie();// decrease shakecounter
                     i--;
             }
         }
     }
 
-    boost::numeric::ublas::vector<Shake> shakes;
+    std::vector<Shake> shakes;
 };
 
 
 short Shake::shakecounter=0;
 
+
 int main()
 {
-    {
-        int log=0;
-        std::cout<<"Log in console? ";
-        std::cin>>log;
+    Game game;
 
-        if (log==0){
-            std::clog.setstate(std::ios_base::failbit);
-        }
-    }
+    game.setup();
 
-    int timedelay;
-    std::cout<<"Enter time delay. ";
-    std::cin>>timedelay;
-    std::cout<<std::endl;
+    game.setupwindow();
+
+    World world;
+
+    Shakescntrl shakescntrl;
+
+    render rendershake(game.getwindow());
 
     srand(time(NULL));
+
     std::string Map [hmap] = {
 	"0000000000000000000000000000000000000000",
 	"0                                      0",
@@ -301,20 +339,6 @@ int main()
 	"0                                      0",
 	"0000000000000000000000000000000000000000",
 };
-    //setFrameLimit(60);
-	//float CurrentFrame = 0;
-
-	//Clock clock;
-
-    //rectangle.setSize(Vector2f(Size, Size));
-    Game game;
-
-    World world;
-
-    Shakescntrl shakescntrl;
-
-    render rendershake(game.getwindow());
-
 
 
 	while (game.getwindow()->isOpen()){
@@ -329,6 +353,10 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				game.getwindow()->close();
+            if (event.mouseButton.button == sf::Mouse::Button::Left)
+                game.settimedelay(game.gettimedelay()+5);
+            if (event.mouseButton.button == sf::Mouse::Button::Right)
+                game.settimedelay(game.gettimedelay()-5);
 		}
 
         std::clog<<std::endl<<std::endl;
@@ -338,9 +366,11 @@ int main()
 
         world.update(Map);
 
+        //Map[0][0]='0';  //need delete
+
         rendershake.gorender(Map, hmap, wmap, Size, Size2);
 
-		Sleep(timedelay);
+		Sleep(game.gettimedelay());
 	}
 	return 0;
 }
